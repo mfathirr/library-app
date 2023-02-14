ff34faabfcc92ae460fb674880870026fe04cac6
MFathirr <muhdyathir623@gmail.com>
2023-02-14 14:51:41 +0700

    fixing ui for edit

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,50 +1,72 @@
 @extends('layouts.app')
 @section('main') 
-<div class="mt-5 mx-auto" style="width: 380px">
-    {{-- @if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-    @endif --}}
-
-    <div class="card">
+<div class="w-full max-w-xs mx-auto w-10 mt-9">
+    <div class="card bg-gray-100">
         <div class="card-body">
-            <form action="{{url("/book/$book->id")}}" method="POST">
+            <form class="w-full max-w-sm" action="{{url("/book/$book->id")}}" method="POST">
                 @csrf
                 @method('PATCH')
-                <div class="mb-3">
-                    <label for="" class="form-label">Title</label>
-                    <input type="text" class="form-control" name="title" value="{{ $book->title }}">
+                <div class="md:flex md:items-center mb-6">
+                  <div class="md:w-1/3">
+                    <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
+                      Title of Book
+                    </label>
+                  </div>
+                  <div class="md:w-2/3">
+                    <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" value="{{ $book->title }}" name="title">
                     @error('title')
-                        <span class="text-danger fw-bold">
+                        <span class="text-red-500 font-semibold">
                             {{ $message }}
                         </span>
                     @enderror
+                  </div>
                 </div>
-                <div class="mb-3">
-                    <label for="" class="form-label">Author</label>
-                    <input type="text" class="form-control"  name="author" value="{{ $book->author }}"></input>
+                <div class="md:flex md:items-center mb-6">
+                  <div class="md:w-1/3">
+                    <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-password">
+                      Author
+                    </label>
+                  </div>
+                  <div class="md:w-2/3">
+                    <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-password" type="text" name="author" value="{{ $book->author }}">
                     @error('author')
-                        <span class="text-danger fw-bold">
+                        <span class="text-red-500 font-semibold">
                             {{ $message }}
                         </span>
                     @enderror
+                  </div>
                 </div>
-                <div class="mb-3">
-                    <label for="" class="form-label">Pages Of Book</label>
-                    <input type="text" class="form-control"  name="pages" value="{{ $book->pages }}"></input>
-                    @error('pages')
-                        <span class="text-danger fw-bold">
-                            {{ $message }}
-                        </span>
-                    @enderror
+                <div class="md:flex md:items-center mb-6">
+                    <div class="md:w-1/3">
+                      <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-password">
+                        Pages of Book
+                      </label>
+                    </div>
+                    <div class="md:w-2/3">
+                      <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-password" type="text" name="pages" value="{{ $book->pages }}">
+                        @error('pages')
+                            <span class="text-red-500 font-semibold">
+                                {{ $message }}
+                            </span>
+                        @enderror
+                    </div>
+                  </div>
+                <div class="md:flex md:items-center">
+                  <div class="md:w-1/3"></div>
+                  <div class="md:w-2/3">
+                    <button class="shadow bg-purple-400 hover:bg-purple-500 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded float-right mt-3" type="submit">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 10.5v6m3-3H9m4.06-7.19l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z" />
+                        </svg>
+                    </button>
+                    <a href="{{url("/book")}}" class="mr-2 shadow bg-red-400 hover:bg-red-500 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded float-right mt-3" type="submit">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
+                          </svg>
+                    </a>
+                  </div>
                 </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
+              </form>
         </div>
     </div>
 </div>
