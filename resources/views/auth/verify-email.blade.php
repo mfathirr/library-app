8eacac8ecfe48cb5cb419d1ce9e182889a1690cc
MFathirr <muhdyathir623@gmail.com>
2023-02-15 14:07:02 +0700

    add verify email

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -8,36 +8,16 @@
                     Verifikasi Email berhasil terkirim
                 </div>
             @endif
+            Before proceeding, please check your email for verification.
+            Or
             <form class="w-full max-w-sm" action="{{route('verification.send')}}" method="POST">
-                @csrf
-                <div class="md:flex md:items-center mb-6">
-                  <div class="md:w-1/3">
-                    <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-password">
-                      Email
-                    </label>
-                  </div>
-                  <div class="md:w-2/3">
-                    <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-password" type="email" name="email" value="{{ old('email') }}">
-                    @error('email')
-                        <span class="text-red-500 font-semibold">
-                            {{ $message }}
-                        </span>
-                    @enderror
-                  </div>
-                </div>
                 <div class="md:flex md:items-center">
                   <div class="md:w-1/3"></div>
                   <div class="md:w-2/3">
+                    @csrf
                     <button class="shadow bg-purple-400 hover:bg-purple-500 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded float-right mt-3" type="submit">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 10.5v6m3-3H9m4.06-7.19l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z" />
-                        </svg>
+                        {{_("click here to request")}}
                     </button>
-                    <a href="{{url("/book")}}" class="mr-2 shadow bg-red-400 hover:bg-red-500 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded float-right mt-3" type="submit">
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
-                      </svg>
-                  </a>
                   </div>
                 </div>
               </form>
