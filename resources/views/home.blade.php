60e2272b60550e7cbe76beb985f3dc74b958f398
MFathirr <muhdyathir623@gmail.com>
2023-02-16 14:32:44 +0700

    add link to welcome

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,9 @@
 <div class="rounded mt-5 mx-auto bg-gradient-to-r from-slate-500 to-slate-400 w-fit outline-0 shadow-lg">
     <div class="d-flex justify-content-between flex-shrink-0 p-3 link-dark  border-bottom">
         @if (Auth::user())
-        <span class="fs-5 fw-semibold">Welcome To Library, {{Auth::user()->name}}!</span>
+        <span class="fs-5 fw-semibold"><a href="{{url("/book")}}">Welcome To Library, {{Auth::user()->name}}!</a></span>
         @else
-        <span class="fs-5 fw-semibold">Welcome To Library!</span>
+        <span class="fs-5 fw-semibold"><a href="{{url("/book")}}">Welcome To Library!</a></span>
         @endif
     </div>
 </div>
