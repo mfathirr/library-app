86d6800ba6714edacb35da01c5be364d5ba56268
MFathirr <muhdyathir623@gmail.com>
2023-03-31 08:59:03 +0700

    edit code for hosting

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -50,16 +50,16 @@ class FortifyServiceProvider extends ServiceProvider
             return view('auth.login');
         });
 
-        Fortify::requestPasswordResetLinkView(function () {
-            return view('auth.forgot-password');
-        });
+        // Fortify::requestPasswordResetLinkView(function () {
+        //     return view('auth.forgot-password');
+        // });
 
-        Fortify::resetPasswordView(function ($request) {
-            return view('auth.reset-password', ['request' => $request]);
-        });
+        // Fortify::resetPasswordView(function ($request) {
+        //     return view('auth.reset-password', ['request' => $request]);
+        // });
 
-        Fortify::verifyEmailView(function(){
-            return view('auth.verify-email');
-        });
+        // Fortify::verifyEmailView(function(){
+        //     return view('auth.verify-email');
+        // });
     }
 }
